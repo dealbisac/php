b27699c00ee1024b7a04e9d7c8ae663c964f80bb
Dipendra Bahadur Chand <chand.dipendra19@gmail.com>
2019-07-29 18:16:10 +0545

    work added

--- project/index.php
+++ project/index.php
@@ -25,6 +25,9 @@ $skillresult = mysqli_query($connection, $skillsql);
 $langaugesql = "SELECT * FROM `language` WHERE `userid`=1";
 $langaugeresult = mysqli_query($connection, $langaugesql);
 
+//fetching the value of workexperience table
+$worksql = "SELECT * FROM `work` WHERE `userid`=1";
+$workresult = mysqli_query($connection, $worksql);
 
 ?>
 <!DOCTYPE html>
@@ -104,23 +107,27 @@ if(mysqli_num_rows($langaugeresult) > 0){
 
 <div class="w3-container w3-card w3-white w3-margin-bottom">
 <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-suitcase fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Work Experience</h2>
-<div class="w3-container">
-<h5 class="w3-opacity"><b>Front End Developer / w3schools.com</b></h5>
-<h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Jan 2015 - <span class="w3-tag w3-teal w3-round">Current</span></h6>
-<p>Lorem ipsum dolor sit amet. Praesentium magnam consectetur vel in deserunt aspernatur est reprehenderit sunt hic. Nulla tempora soluta ea et odio, unde doloremque repellendus iure, iste.</p>
-<hr>
-</div>
-<div class="w3-container">
-<h5 class="w3-opacity"><b>Web Developer / something.com</b></h5>
-<h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Mar 2012 - Dec 2014</h6>
-<p>Consectetur adipisicing elit. Praesentium magnam consectetur vel in deserunt aspernatur est reprehenderit sunt hic. Nulla tempora soluta ea et odio, unde doloremque repellendus iure, iste.</p>
-<hr>
-</div>
-<div class="w3-container">
-<h5 class="w3-opacity"><b>Graphic Designer / designsomething.com</b></h5>
-<h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Jun 2010 - Mar 2012</h6>
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p><br>
-</div>
+<?php
+if(mysqli_num_rows($workresult)>0){
+  while($row = mysqli_fetch_assoc($workresult)){
+    echo "<div class='w3-container'>";
+    echo "<h5 class='w3-opacity'><b>".$row["position"]." - ".$row["company"]."</b></h5>";
+    echo "<h6 class='w3-text-teal'><i class='fa fa-calendar fa-fw w3-margin-right'></i>".$row["startdate"]." - "; 
+    if($row["enddate"]==NULL){
+      echo "<span class='w3-tag w3-teal w3-round'>Current</span></h6>";
+    }else{
+      echo $row["enddate"]."</span></h6>";
+    }
+    echo "<p>".$row["description"]."</p>";
+    echo "<hr></div>";
+  }
+
+
+}else{
+  echo "No results found";
+}
+
+?>
 </div>
 
 <div class="w3-container w3-card w3-white">
